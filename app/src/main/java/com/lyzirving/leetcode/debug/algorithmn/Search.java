84fcdae0be26abb0a8e2707addae73f702099acf
liuyuzhou <lyzirving@sina.com>
2022-04-25 13:57:46 +0800

    LeetCode No.35: searchInsert;
    Signed-off-by: liuyuzhou <lyzirving@sina.com>

--- app/src/main/java/com/lyzirving/leetcode/debug/algorithmn/Search.java
+++ app/src/main/java/com/lyzirving/leetcode/debug/algorithmn/Search.java
@@ -57,4 +57,53 @@ public class Search {
         right--;
         return new int[]{left, right};
     }
+
+    private static int binarySearchOrInsert(int left, int right, int[] nums, int target) {
+        if(left == right) {
+            if (nums[left] == target)
+                return left;
+            else {
+                if (nums[left] < target)
+                    return left + 1;
+                else
+                    return 0;
+            }
+        }
+        int mid = (left + right) >> 1;
+        if(mid == left) {
+            if(nums[left] == target)
+                return left;
+            else if(nums[right] == target)
+                return right;
+            return left + 1;
+        }
+        if(target < nums[mid]) {
+            right = mid;
+        } else if(target > nums[mid]) {
+            left = mid;
+        } else {
+            return mid;
+        }
+        return binarySearchOrInsert(left, right, nums, target);
+    }
+
+    /**
+     * LeetCode question No.35
+     * find target in nums which is in ascending order;
+     * if found, return its's position; if not, return it's insertion position
+     * @param nums input array
+     * @param target target value
+     * @return specific position
+     */
+    public static int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return -1;
+        if (nums[0] >= target)
+            return 0;
+        if (nums[nums.length - 1] < target)
+            return nums.length;
+        if (nums[nums.length - 1] == target)
+            return nums.length - 1;
+        return binarySearchOrInsert(0, nums.length - 1, nums, target);
+    }
 }
